380bebed786ec30a902030faa14c8f273387f679
giovannibbianco <2038962B@student.gla.ac.uk>
2020-10-13 14:13:30 +0100

    correlation test

--- correlation_test.R
+++ correlation_test.R
@@ -1,10 +1,15 @@
-# correlation test NDVI 
+# correlation test time series matrices 
+
 delta<-read.csv("Delta_NDVI.csv")
 ndvi<-read.csv("ndvi_matrix.csv")
 anomaly<-read.csv("anomaly_ndvi_matrix.csv")
 grass<-read.csv("grass_matrix.csv")
+rain<-read.csv("rain_matrix.csv")
 
 cor(ndvi,grass)
 cor(ndvi,delta)
 cor(ndvi,anomaly)
 cor(anomaly,delta)
+cor(rain,grass)
+cor(rain,ndvi)
+cor(rain,anomaly)
