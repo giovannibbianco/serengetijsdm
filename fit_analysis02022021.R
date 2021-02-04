3cb19419d4a5574d4a09329c8effc6d426fdb93c
giovannibbianco <2038962B@student.gla.ac.uk>
2021-02-04 15:40:27 +0100

    first look at results

---
+++ fit_analysis02022021.R
@@ -0,0 +1,28 @@
+# results 2-2-2021
+# model used: poisson_binomial_dates_pobs_se1
+
+
+library(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+load("ser.RData") # in environment it is read as fits
+
+fit_summary <- summary(fits)$summary 
+
+# responses to environmental variables
+responses <- fit_summary[grepl("b_m", rownames(fit_summary)),]
+which(sign(responses[,4])==sign(responses[,8])) # which ones do not cross the 0 line with their credible interval 
+
+# traits
+traits_effects<-fit_summary[grepl("Z", rownames(fit_summary)),]
+which(sign(traits_effects[,4])==sign(traits_effects[,8]))
+
+#r-hat and effective sample size
+hist(fit_summary[,10], main = "R-hat Std Model")
+hist(fit_summary[,9], main = "n-eff Std Model" )
+
+# phylogenetic signal 
+draws <- extract(fits, pars = "rho")
+plot(density(draws$rho), main="Rho-Phylogenetic Signal")
+
---
+++ plots_02022021.R
@@ -0,0 +1,301 @@
+# variables for plots
+
+#  13 environmental vars + intercept 
+
+Xo <- as.matrix(read.csv("covariates_final.csv"))
+varnames<-c(colnames(Xo),"rain","grass","ndvi","a_ndvi","d_ndvi")
+
+#traits
+TT<-read.csv("TT_matrix.csv")
+names(TT)
+
+# select different coefs
+
+intercepts<-as.data.frame(responses[seq(length.out=19, from=1, by=14),])
+village_coefs<-as.data.frame(responses[seq(length.out=19, from=2, by=14),])
+tourism_coefs<-as.data.frame(responses[seq(length.out=19, from=3, by=14),])
+drainages_coefs<-as.data.frame(responses[seq(length.out=19, from=4, by=14),])
+woodycover_coefs<-as.data.frame(responses[seq(length.out=19, from=5, by=14),])
+nitrogen_coefs<-as.data.frame(responses[seq(length.out=19, from=6, by=14),])
+runoff_coefs<-as.data.frame(responses[seq(length.out=19, from=7, by=14),])
+fire_coefs<-as.data.frame(responses[seq(length.out=19, from=8, by=14),])
+water_coefs<-as.data.frame(responses[seq(length.out=19, from=9, by=14),])
+rain_coefs<-as.data.frame(responses[seq(length.out=19, from=10, by=14),])
+grass_coefs<-as.data.frame(responses[seq(length.out=19, from=11, by=14),])
+ndvi_coefs<-as.data.frame(responses[seq(length.out=19, from=12, by=14),])
+andvi_coefs<-as.data.frame(responses[seq(length.out=19, from=13, by=14),])
+dndvi_coefs<-as.data.frame(responses[seq(length.out=19, from=14, by=14),])
+
+sp.names<-as.vector(TT[,2])
+
+
+# Intercept Plot
+
+ggplot(intercepts, aes(x=sp.names,y=mean))+
+  geom_point(size=2)+
+  geom_hline(yintercept = 0, colour="grey",linetype=2, lwd=1)+
+  geom_errorbar(ymin=as.numeric(intercepts[,4]),
+                ymax=as.numeric(intercepts[,8]),width=0.4, size=0.8)+
+  theme_bw()+
+  xlab("Herbivore Species")+
+  ggtitle("Intercepts")+
+  ylab("Parameter estimate")+
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+# Village Distance Plot
+
+ggplot(village_coefs, aes(x=sp.names,y=mean))+
+  geom_point(size=2)+
+  geom_hline(yintercept = 0, colour="grey",linetype=2, lwd=1)+
+  geom_errorbar(ymin=as.numeric(village_coefs[,4]),
+                ymax=as.numeric(village_coefs[,8]),width=0.4, size=0.8)+
+  theme_bw()+
+  xlab("Herbivore Species")+
+  ggtitle("Species Responses to Distance from Villages")+
+  ylab("Parameter estimate")+
+  ylim(-1,1.5)+
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+
+
+# Tourism Plot 
+
+ggplot(tourism_coefs, aes(x=sp.names,y=mean))+
+  geom_point(size=2)+
+  geom_hline(yintercept = 0, colour="grey",linetype=2, lwd=1)+
+  geom_errorbar(ymin=as.numeric(tourism_coefs[,4]),
+                ymax=as.numeric(tourism_coefs[,8]),width=0.4, size=0.8)+
+  theme_bw()+
+  xlab("Herbivore Species")+
+  ggtitle("Species Responses to Tourism")+
+  ylab("Parameter estimate")+
+  ylim(-1,1.5)+
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+# Drainages plot
+
+ggplot(drainages_coefs, aes(x=sp.names,y=mean))+
+  geom_point(size=2)+
+  geom_hline(yintercept = 0, colour="grey",linetype=2, lwd=1)+
+  geom_errorbar(ymin=as.numeric(drainages_coefs[,4]),
+                ymax=as.numeric(drainages_coefs[,8]),width=0.4, size=0.8)+
+  theme_bw()+
+  xlab("Herbivore Species")+
+  ggtitle("Species Responses to distance from drainages")+
+  ylab("Parameter estimate")+
+  ylim(-1,1)+
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+#woodycover plot
+
+ggplot(woodycover_coefs,aes(x=sp.names,y=mean))+
+  geom_point(size=2)+
+  geom_hline(yintercept = 0, colour="grey",linetype=2, lwd=1)+
+  geom_errorbar(ymin=as.numeric(woodycover_coefs[,4]),
+                ymax=as.numeric(woodycover_coefs[,8]),width=0.4, size=0.8)+
+  theme_bw()+
+  xlab("Herbivore Species")+
+  ggtitle("Species Responses to density of tree cover")+
+  ylab("Parameter estimate")+
+  ylim(-1,1)+
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+#nitrogen plot
+
+
+ggplot(nitrogen_coefs,aes(x=sp.names,y=mean))+
+  geom_point(size=2)+
+  geom_hline(yintercept = 0, colour="grey",linetype=2, lwd=1)+
+  geom_errorbar(ymin=as.numeric(nitrogen_coefs[,4]),
+                ymax=as.numeric(nitrogen_coefs[,8]),width=0.4, size=0.8)+
+  theme_bw()+
+  xlab("Herbivore Species")+
+  ggtitle("Species Responses to distance from N hotspot")+
+  ylab("Parameter estimate")+
+  ylim(-1,1)+
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+# runoff plot
+
+ggplot(runoff_coefs,aes(x=sp.names,y=mean))+
+  geom_point(size=2)+
+  geom_hline(yintercept = 0, colour="grey",linetype=2, lwd=1)+
+  geom_errorbar(ymin=as.numeric(runoff_coefs[,4]),
+                ymax=as.numeric(runoff_coefs[,8]),width=0.4, size=0.8)+
+  theme_bw()+
+  xlab("Herbivore Species")+
+  ggtitle("Species Responses to water runoff")+
+  ylab("Parameter estimate")+
+  ylim(-1,1)+
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+# fire plot
+
+ggplot(fire_coefs,aes(x=sp.names,y=mean))+
+  geom_point(size=2)+
+  geom_hline(yintercept = 0, colour="grey",linetype=2, lwd=1)+
+  geom_errorbar(ymin=as.numeric(fire_coefs[,4]),
+                ymax=as.numeric(fire_coefs[,8]),width=0.4, size=0.8)+
+  theme_bw()+
+  xlab("Herbivore Species")+
+  ggtitle("Species Responses to frequency of fires")+
+  ylab("Parameter estimate")+
+  ylim(-1,1)+
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+# water plot
+
+ggplot(water_coefs,aes(x=sp.names,y=mean))+
+  geom_point(size=2)+
+  geom_hline(yintercept = 0, colour="grey",linetype=2, lwd=1)+
+  geom_errorbar(ymin=as.numeric(water_coefs[,4]),
+                ymax=as.numeric(water_coefs[,8]),width=0.4, size=0.8)+
+  theme_bw()+
+  xlab("Herbivore Species")+
+  ggtitle("Species Responses to distance from permanent water sources")+
+  ylab("Parameter estimate")+
+  ylim(-4,4)+
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+# rain plot
+
+ggplot(rain_coefs,aes(x=sp.names,y=mean))+
+  geom_point(size=2)+
+  geom_hline(yintercept = 0, colour="grey",linetype=2, lwd=1)+
+  geom_errorbar(ymin=as.numeric(rain_coefs[,4]),
+                ymax=as.numeric(rain_coefs[,8]),width=0.4, size=0.8)+
+  theme_bw()+
+  xlab("Herbivore Species")+
+  ggtitle("Species Responses to precipitations levels")+
+  ylab("Parameter estimate")+
+  ylim(-1,1)+
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+# grass abundance
+
+
+ggplot(grass_coefs,aes(x=sp.names,y=mean))+
+  geom_point(size=2)+
+  geom_hline(yintercept = 0, colour="grey",linetype=2, lwd=1)+
+  geom_errorbar(ymin=as.numeric(grass_coefs[,4]),
+                ymax=as.numeric(grass_coefs[,8]),width=0.4, size=0.8)+
+  theme_bw()+
+  xlab("Herbivore Species")+
+  ggtitle("Species Responses to grass height")+
+  ylab("Parameter estimate")+
+  ylim(-1,1)+
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+# ndvi plot
+
+ggplot(ndvi_coefs,aes(x=sp.names,y=mean))+
+  geom_point(size=2)+
+  geom_hline(yintercept = 0, colour="grey",linetype=2, lwd=1)+
+  geom_errorbar(ymin=as.numeric(ndvi_coefs[,4]),
+                ymax=as.numeric(ndvi_coefs[,8]),width=0.4, size=0.8)+
+  theme_bw()+
+  xlab("Herbivore Species")+
+  ggtitle("Species Responses to NDVI")+
+  ylab("Parameter estimate")+
+  ylim(-100,100)+
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+
+# andvi plot
+
+ggplot(andvi_coefs,aes(x=sp.names,y=mean))+
+  geom_point(size=2)+
+  geom_hline(yintercept = 0, colour="grey",linetype=2, lwd=1)+
+  geom_errorbar(ymin=as.numeric(andvi_coefs[,4]),
+                ymax=as.numeric(andvi_coefs[,8]),width=0.4, size=0.8)+
+  theme_bw()+
+  xlab("Herbivore Species")+
+  ggtitle("Species Responses to NDVI")+
+  ylab("Parameter estimate")+
+  ylim(-100,100)+
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
+
+
+# dndvi plot
+
+ggplot(dndvi_coefs,aes(x=sp.names,y=mean))+
+  geom_point(size=2)+
+  geom_hline(yintercept = 0, colour="grey",linetype=2, lwd=1)+
+  geom_errorbar(ymin=as.numeric(dndvi_coefs[,4]),
+                ymax=as.numeric(dndvi_coefs[,8]),width=0.4, size=0.8)+
+  theme_bw()+
+  xlab("Herbivore Species")+
+  ggtitle("Species Responses to NDVI")+
+  ylab("Parameter estimate")+
+  ylim(-100,100)+
+  theme(axis.text.x=element_text(angle=45, hjust=1))+
+  theme(axis.line = element_line(colour = "black"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank())
